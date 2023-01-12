78414cd7402f5dbaf3f784eeedfc239b4c2cc191
NaufalRizqullah23 <naufalrizqullah23@gmail.com>
2023-01-12 15:18:31 +0700

    slicing login page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['"Nunito Sans"'],
+      },
+      colors: {
+        aBlue: "#1F5F8B",
+        bBlue: "#334777",
+        cBlue: "#65A8CA",
+        dBlue: "#1891AC",
+        lBlue: "#D2ECF9",
+      },
+    },
+  },
+  plugins: [],
+};
